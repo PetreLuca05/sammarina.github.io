33f24266e28fb0e575bdfe259983bb6181e50399
PetreLuca05 <121764994+PetreLuca05@users.noreply.github.com>
2023-12-18 22:55:31 +0300

    ewew

--- anunturi/anunturi.js
+++ anunturi/anunturi.js
@@ -1,3 +1,4 @@
+
 function ElementFromHtml(html){
     const template = document.createElement("template");
 
@@ -10,13 +11,20 @@ const ul = document.querySelector(".lista-spatii ul");
 const numOfDivs = 100;
 
 for(let i = 0; i < numOfDivs; i++){
+
+    const filter = "mamaia";
+    const imageURL = '../img/s/s2.jpg';
+    const strada = 'XXX ';
+    const numar = 'YYY ';
+    const detaliiii = 'detalii legate de spatiu ( mp, suprafata - utila, etc)'
+
     const myList = ElementFromHtml(`
-    <li>
-        <section></section>
+    <li data-filter = "${filter}">
+        <section style="background-image: url(${imageURL});"></section>
 
         <section>
-            <h1>strada x, numarul y</h1>
-            <p>detalii ( mp, suprafata - utila, etc. )</p>
+            <h1>strada ${strada}, numarul ${numar}</h1>
+            <p>${detaliiii}</p>
 
             <a href="">vizualizeaza</a>
         </section>
@@ -26,4 +34,28 @@ for(let i = 0; i < numOfDivs; i++){
     ul.appendChild(myList);
 }
 
+const liItem = document.querySelectorAll(".filtre p");
+const imgItem = document.querySelectorAll(".lista-spatii ul li");
 
+liItem.forEach(li => {
+    li.onclick = function() {
+        //active
+        liItem.forEach(li => {
+            li.className = "";
+        })
+        li.className = "selected";
+
+        const value= li.textContent;
+        imgItem.forEach(img => {
+            img.style.display = "none";
+            //console.log(value);
+    
+            if(img.getAttribute("data-filter") == value.toLocaleLowerCase()){
+                img.style.display = "block";
+            }
+        })
+    }
+})
+
+//let workbook = XLSX.read(await (await fetch("spatii.xlsx")).arrayBuffer());
+//console.log(workbook);
