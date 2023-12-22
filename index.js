6a9b602612d1b7bcdfade752910219643ae12ce2
PetreLuca05 <121764994+PetreLuca05@users.noreply.github.com>
2023-12-22 23:54:35 +0300

    k

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function ElementFromHtml(html){
     return template.content.firstElementChild;
 }
 
+/*
 const ul = document.querySelector(".unu");
 const numOfDivs = 9;
 
@@ -24,5 +25,37 @@ for(let i = 0; i < numOfDivs; i++){
 
     ul.appendChild(myList);
 }
+*/
 
 
+GenerateList()
+function GenerateList(){
+const ul = document.querySelector(".unu");
+fetch('anunturi/spatii.xlsx')
+  .then(response => response.blob())
+  .then(blob => readXlsxFile(blob))
+  .then((rows) => {
+    for(let i = 1; i < 10; i++){
+        const filter = rows[i][2];
+
+        const imageURL = rows[i][4];
+        const strada = rows[i][0];
+        const numar = rows[i][1];
+        const detaliiii = rows[i][3];
+    
+        const myList = ElementFromHtml(`
+
+        <div style = " background-image: linear-gradient(180deg, rgba(20, 20, 20, .25) 50px, rgba(20, 20, 20, 1)), url(${imageURL});">
+            <section>
+                <h1>strada ${strada}, numarul ${numar} <i class="fa-solid fa-location-dot"></i></h1>
+            </section>
+
+            <p>${detaliiii}</p>
+        </div>  
+        
+        `)
+    
+        ul.appendChild(myList);
+    }
+  })
+}
